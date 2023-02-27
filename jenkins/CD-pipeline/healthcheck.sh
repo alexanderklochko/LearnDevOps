3552147462c8a840aa03646559bbbb10b71e3b45
Oleksandr Klochko <Oleksandr_Klochko@epam.com>
2023-02-27 17:21:17 +0200

    add Jenkinsfile for CD

--- jenkins/CD-pipeline/healthcheck.sh
+++ jenkins/CD-pipeline/healthcheck.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# log to dockerhub and push 
+
+
+
 # Run a curl against the Jenkins instance installed in a Dockerfile
 # to do a basic health check
 
@@ -13,7 +17,7 @@ for ATTEMPT in $(seq ${ATTEMPTS}); do
     echo "Attempt ${ATTEMPT} of ${ATTEMPTS}"
     echo "Curling against the Jenkins server"
     echo "Should expect a 200 within ${CURL_MAX_TIME} seconds"
-    STATUS_CODE=$(curl -sL -w "%{http_code}" localhost:8080 -o /dev/null \
+    STATUS_CODE=$(curl -sL -w "%{http_code}" ${SERVER_IP}:8080 -o /dev/null \
         --max-time ${CURL_MAX_TIME})
 
     if [[ "$STATUS_CODE" == "200" ]]; then
